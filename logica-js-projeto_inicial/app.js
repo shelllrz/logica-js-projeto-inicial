dc605f1fc05abbd2b155fa86065a7abb6455e7c7
Michelle <michellealejanddrina45@gmail.com>
2026-02-05 21:58:42 -0300

    primeiro commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,23 @@
+alert('Boas vindas ao nosso site!');
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+let nomedoUsuario = prompt('Qual é seu nome?');
+if (nomedoUsuario == '') {
+  alert(mensagemDeErro);
+} else {
+  alert('Tudo certo!');
+}
+let idadedoUsuario = prompt('Informe sua idade');
+if (idadedoUsuario == '') {
+  alert(mensagemDeErro);
+} else {
+  alert('Tudo certo!');
+}
+if (idadedoUsuario >= 18) {
+  alert('Já pode tirar habilitação');
+} else {
+    alert('Não pode tirar habilitação');
+}
